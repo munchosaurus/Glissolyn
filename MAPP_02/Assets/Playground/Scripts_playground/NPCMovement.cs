1e5f6ed926aa9e211ff0e52b52fe32937419d09d
munchosaurus <martinkings1@live.se>
2021-04-19 10:58:28 +0200

    Added NPC movement

--- MAPP_02/Assets/Playground/Scripts_playground/NPCMovement.cs
+++ MAPP_02/Assets/Playground/Scripts_playground/NPCMovement.cs
@@ -2,43 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid_movement : MonoBehaviour
+public class NPCMovement : MonoBehaviour
 {
     private bool isMoving;
     private Vector3 originalPos, targetPos;
-    private float timeToMove = 0.3f;
+    private float timeToMove = 1.2f;
+    private bool isTalking;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !isMoving)
+        int movementNumber = randomNumber();
+        if (movementNumber == 5 && !isMoving)
             StartCoroutine(MovePlayer(Vector3.up));
 
-        if (Input.GetKey(KeyCode.A) && !isMoving)
+        if (movementNumber == 50 && !isMoving)
             StartCoroutine(MovePlayer(Vector3.left));
 
-        if (Input.GetKey(KeyCode.S) && !isMoving)
+        if (movementNumber == 100 && !isMoving)
             StartCoroutine(MovePlayer(Vector3.down));
 
-        if (Input.GetKey(KeyCode.D) && !isMoving)
+        if (movementNumber == 150 && !isMoving)
             StartCoroutine(MovePlayer(Vector3.right));
 
     }
 
-    private IEnumerator MovePlayer(Vector3 direction) {
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
         isMoving = true;
         float elapsedTime = 0;
         originalPos = transform.position;
         targetPos = originalPos + direction;
-        
 
-        while (elapsedTime < timeToMove) {
+        while (elapsedTime < timeToMove)
+        {
             transform.position = Vector3.Lerp(originalPos, targetPos, (elapsedTime / timeToMove));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
-        //transform.position = targetPos;
-
+        transform.position = targetPos;
         isMoving = false;
     }
+
+    private int randomNumber() {
+        int number = Random.Range(1, 1000);
+        return number;
+    }
+
+    private void talking() { 
+        
+    }
+   
 }
