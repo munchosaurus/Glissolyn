c3b311655cfcf123f823e190f0777f65224a307c
munchosaurus <martinkings1@live.se>
2021-04-20 13:17:48 +0200

    Animations

--- MAPP_02/Assets/Playground/Scripts_playground/Grid_movement.cs
+++ MAPP_02/Assets/Playground/Scripts_playground/Grid_movement.cs
@@ -5,65 +5,100 @@ using UnityEngine;
 public class Grid_movement : MonoBehaviour
 {
     private bool isMoving;
+    private float timeToMove = 0.35f;
     private Vector3 originalPos;
-    private float timeToMove = 0.3f;
     public bool directionPossible;
     public bool isTalking;
     public LayerMask wallLayer;
     public GameObject rotation;
     [SerializeField] private Animator playerAnimator;
+    private Vector3 currentFacing;
+    private Vector3 direction;
 
     void Update() 
     {
         if (!isTalking)
-        {
-            if (Input.GetKey(KeyCode.W) && !isMoving ) {
-                rotation.transform.position = transform.position + Vector3.up;
-                playerAnimator.SetTrigger("facingUp");
-                StartCoroutine(MovePlayer(rotation.transform.position));
-
+        {   
+            if (Input.GetKey(KeyCode.W) && !isMoving) {
+                direction = Vector3.up;
+                StartCoroutine(MovePlayer());
             }
 
-            if (Input.GetKey(KeyCode.A) && !isMoving ) {
-                rotation.transform.position = transform.position + Vector3.left;
-                playerAnimator.SetTrigger("facingLeft");
-                StartCoroutine(MovePlayer(rotation.transform.position));
-
+            else if (Input.GetKey(KeyCode.A) && !isMoving) {
+                direction = Vector3.left;
+                StartCoroutine(MovePlayer());
             }
 
-            if (Input.GetKey(KeyCode.S) && !isMoving ) {
-                rotation.transform.position = transform.position + Vector3.down;
-                playerAnimator.SetTrigger("facingDown");
-                StartCoroutine(MovePlayer(rotation.transform.position));
-
+            else if (Input.GetKey(KeyCode.S) && !isMoving) {
+                direction = Vector3.down;
+                StartCoroutine(MovePlayer());
             }
 
-            if (Input.GetKey(KeyCode.D) && !isMoving ) {
-                rotation.transform.position = transform.position + Vector3.right;
-                playerAnimator.SetTrigger("facingRight");
-                StartCoroutine(MovePlayer(rotation.transform.position));
+            else if (Input.GetKey(KeyCode.D) && !isMoving) {
+                direction = Vector3.right;
+                StartCoroutine(MovePlayer());
+            }
 
+            else if (!isMoving && currentFacing != direction) {
+                Rotate(direction);
             }
         }
     }
 
-    private IEnumerator MovePlayer(Vector3 direction) {
-        float elapsedTime = 0;
-        if (IsWalkable(direction))
-        {
-            isMoving = true;
-            
+    private IEnumerator MovePlayer() {
+        if (IsWalkable(transform.position + direction)) {
+            float elapsedTime = 0;
             originalPos = transform.position;
+            if (direction == Vector3.up)
+            {
+                playerAnimator.SetTrigger("moveUp");
+            }
+            else if (direction == Vector3.down)
+            {
+                playerAnimator.SetTrigger("moveDown");
+            }
+            else if (direction == Vector3.right)
+            {
+                playerAnimator.SetTrigger("moveRight");
+            }
+            else if (direction == Vector3.left)
+            {
+                playerAnimator.SetTrigger("moveLeft");
+            }
+            isMoving = true;
             while (elapsedTime < timeToMove)
             {
-                transform.position = Vector3.Lerp(originalPos, direction, (elapsedTime / timeToMove));
+                transform.position = Vector3.Lerp(originalPos, originalPos + direction, elapsedTime / timeToMove);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
-            transform.position = direction;
-            isMoving = false;
+            transform.position = originalPos + direction;
+            currentFacing = Vector3.zero;
+            
         }
-        playerAnimator.SetTrigger("stopMoving");
+        isMoving = false;
+    }
+
+    private void Rotate(Vector3 direction) {
+
+        if (direction == Vector3.up) {
+            playerAnimator.SetTrigger("faceUp");
+            
+        }
+        else if (direction == Vector3.down) {
+            playerAnimator.SetTrigger("faceDown");
+        }
+        else if (direction == Vector3.right)
+        {
+            playerAnimator.SetTrigger("faceRight");
+        }
+        else if (direction == Vector3.left)
+        {
+            playerAnimator.SetTrigger("faceLeft");
+        }
+        //playerAnimator.SetTrigger("stopMoving");
+        currentFacing = direction;
+        rotation.transform.position = transform.position + direction;
     }
 
     private bool IsWalkable(Vector3 targetPos) {
