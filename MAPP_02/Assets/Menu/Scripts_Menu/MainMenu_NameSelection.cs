74bf35eb37e899a72b633db8d14681fc517725cc
munchosaurus <martinkings1@live.se>
2021-04-27 09:44:18 +0200

    Added name selection plus 8-bit font to Start menu

---
+++ MAPP_02/Assets/Menu/Scripts_Menu/MainMenu_NameSelection.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu_NameSelection : MonoBehaviour
+{
+    public string playerName;
+    [SerializeField] private GameObject inputField;
+    [SerializeField] private GameObject textDisplay;
+    [SerializeField] private GameObject enterButtonToHide;
+    [SerializeField] private GameObject startGameButton;
+
+
+    //Sets the playername in the welcome screen 
+    public void SetName() {
+        playerName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + playerName + "!" +
+            "Press ENTER WORLD to start exploring the world of Glyssolin!";
+
+        enterButtonToHide.SetActive(false);
+        startGameButton.SetActive(true);
+
+    }
+
+}
