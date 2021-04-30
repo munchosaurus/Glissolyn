02edf18d294f543243cce92cde58e9838831e783
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2021-04-30 12:55:47 +0200

    fixes

--- MAPP_02/Assets/Playground/Scripts_playground/Grid_movement.cs
+++ MAPP_02/Assets/Playground/Scripts_playground/Grid_movement.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class Grid_movement : MonoBehaviour
 {
-<<<<<<< HEAD
     private bool isMoving;
     private Vector3 originalPos, targetPos;
     private float timeToMove = 0.3f;
-=======
-    [SerializeField] private LayerMask wallLayer;
-    [SerializeField] private GameObject rotation;
-    [SerializeField] private Animator playerAnimator;
-
-    private bool isMoving;
-    private float timeToMove = 0.35f;
-    private Vector3 originalPos;
-    private bool shouldMove;
-    private Vector3 currentFacing = Vector3.zero;
-    private Vector3 direction = Vector3.zero;
->>>>>>> Main
 
     void Update()
     {
-<<<<<<< HEAD
         if (Input.GetKey(KeyCode.W) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.up));
 
@@ -43,100 +29,6 @@ public class Grid_movement : MonoBehaviour
         float elapsedTime = 0;
         originalPos = transform.position;
         targetPos = originalPos + direction;
-=======
-        if (!Game_Controller.IsGamePaused() && !Game_Controller.IsCombatActive())
-        {
-            /*if (Input.GetKey(KeyCode.W) && !isMoving) {
-                direction = Vector3.up;
-                StartCoroutine(MovePlayer(direction));
-            }
-
-            else if (Input.GetKey(KeyCode.A) && !isMoving) {
-                direction = Vector3.left;
-                StartCoroutine(MovePlayer(direction));
-            }
-
-            else if (Input.GetKey(KeyCode.D) && !isMoving)
-            {
-                direction = Vector3.right;
-                StartCoroutine(MovePlayer(direction));
-            }
-
-            else if (Input.GetKey(KeyCode.S) && !isMoving) {
-                direction = Vector3.down;
-                StartCoroutine(MovePlayer(direction));
-            }
-
-            if (!isMoving && currentFacing != direction) {
-                Rotate(direction);
-            }*/
-            if (shouldMove && !isMoving)
-            {
-                StartCoroutine(MovePlayer(direction));
-            }
-
-            if (!isMoving && currentFacing != direction)
-            {
-                Rotate(direction);
-            }
-        }
-    }
-
-    private IEnumerator MovePlayer(Vector3 direction) {
-        if (IsWalkable(transform.position + direction)) {
-            float elapsedTime = 0;
-            originalPos = transform.position;
-            if (direction == Vector3.up)
-            {
-                playerAnimator.SetTrigger("moveUp");
-            }
-            else if (direction == Vector3.down)
-            {
-                playerAnimator.SetTrigger("moveDown");
-            }
-            else if (direction == Vector3.right)
-            {
-                playerAnimator.SetTrigger("moveRight");
-            }
-            else if (direction == Vector3.left)
-            {
-                playerAnimator.SetTrigger("moveLeft");
-            }
-            isMoving = true;
-            rotation.transform.position = transform.position + direction;
-            while (elapsedTime < timeToMove)
-            {
-                transform.position = Vector3.Lerp(originalPos, originalPos + direction, elapsedTime / timeToMove);
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-            transform.position = originalPos + direction;
-            currentFacing = Vector3.zero;
-        }
-        isMoving = false;
-    }
-
-    private void Rotate(Vector3 direction) {
-
-        if (direction == Vector3.up) {
-            playerAnimator.SetTrigger("faceUp");
-            
-        }
-        else if (direction == Vector3.down) {
-            playerAnimator.SetTrigger("faceDown");
-        }
-        else if (direction == Vector3.right)
-        {
-            playerAnimator.SetTrigger("faceRight");
-        }
-        else if (direction == Vector3.left)
-        {
-            playerAnimator.SetTrigger("faceLeft");
-        }
-        currentFacing = direction;
-        rotation.transform.position = transform.position + direction;
-    }
->>>>>>> Main
 
         while (elapsedTime < timeToMove) {
             transform.position = Vector3.Lerp(originalPos, targetPos, (elapsedTime / timeToMove));
@@ -146,41 +38,6 @@ public class Grid_movement : MonoBehaviour
 
         transform.position = targetPos;
 
-<<<<<<< HEAD
         isMoving = false;
-=======
-    public Vector3 GetFacing()
-    {
-        return currentFacing;
-    }
-
-    public void MoveUp()
-    {
-        direction = Vector3.up;
-        shouldMove = true;
-    }
-
-    public void MoveLeft()
-    {
-        direction = Vector3.left;
-        shouldMove = true;
-    }
-
-    public void MoveRight()
-    {
-        direction = Vector3.right;
-        shouldMove = true;
-    }
-
-    public void MoveDown()
-    {
-        direction = Vector3.down;
-        shouldMove = true;
-    }
-
-    public void StopMoving()
-    {
-        shouldMove = false;
->>>>>>> Main
     }
 }
--- MAPP_02/Assets/Playground/Scripts_playground/QuestLog.cs
+++ MAPP_02/Assets/Playground/Scripts_playground/QuestLog.cs
@@ -8,66 +8,11 @@ public class QuestLog : MonoBehaviour
     [SerializeField] private Button questButtonPrefab;
 
     private Dictionary<int, Quest> activeQuests = new Dictionary<int, Quest>();
-    private QuestButton currentOpenQuestButton;
 
     public void AddQuest(Quest quest)
     {
         activeQuests.Add(quest.GetQuestID(), quest);
-<<<<<<< HEAD
         Button theQuestButton = Instantiate(questButtonPrefab, transform.Find("Quest List Area").transform.Find("Viewport").transform.Find("Content"));
         theQuestButton.GetComponent<QuestButton>().Initialize(quest.GetQuestTitle(), quest.GetQuestText(), quest.GetQuestID());
-=======
-        Button theQuestButton = Instantiate(questButtonPrefab, transform.Find("Quest List Area").transform.Find("Viewport").transform.Find("Content")); // Create a new Quest Button prefeb as a child to the "Content"-GameObject under the "Quest List Area"-GameObject.
-        theQuestButton.GetComponent<QuestButton>().Initialize(quest); // Add the information from the Quest-object to the "Quest Button"-GameObject.
-    }
-
-    public void RemoveQuest(int id)
-    {
-        if (activeQuests.ContainsKey(id))
-        {
-            Destroy(gameObject.transform.Find(activeQuests[id].GetQuestTitle()));
-            activeQuests.Remove(id);
-        }
-    }
-
-    public bool HasQuest(Quest quest)
-    {
-        return activeQuests.ContainsKey(quest.GetQuestID());
-    }
-
-    public void UpdateKillQuestsWithEnemyType(CharacterBase _base)
-    { 
-        foreach(KeyValuePair<int, Quest> entry in activeQuests)
-        {
-            if(entry.Value.GetQuestType() == QuestType.KILL_QUEST)
-            {
-                Quest_KillQuest q = entry.Value as Quest_KillQuest;
-                if(q.GetEnemyToKill() == _base)
-                {
-                    q.UpdateQuest();
-                }
-            }
-        }
-    }
-
-    public void SetCurrentOpenQuest(QuestButton questButton)
-    {
-        currentOpenQuestButton = questButton;
-    }
-
-    public void Toggle()
-    {
-        if (currentOpenQuestButton != null)
-        {
-            currentOpenQuestButton.OnClick();
-        }
-        gameObject.SetActive(!gameObject.activeInHierarchy);
-        Game_Controller.TogglePause(gameObject.activeInHierarchy);
-    }
-
-    public bool IsOpen()
-    {
-        return gameObject.activeInHierarchy;
->>>>>>> Main
     }
 }
