1e6a7739c1953a20cc3d8e0f7d0cc7c1b94887fc
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2021-04-30 13:07:59 +0200

    Revert "e"
    This reverts commit c32580aa3f74abb092e5fa2edc308f4804100222.

--- MAPP_02/Assets/Playground/Scripts_playground/Quest.cs
+++ MAPP_02/Assets/Playground/Scripts_playground/Quest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-<<<<<<< HEAD
 public class Quest
 {
     private static int QUEST_ID_COUNTER = 0;
@@ -17,22 +16,6 @@ public class Quest
         this.questText = questText;
         this.questID = QUEST_ID_COUNTER;
         QUEST_ID_COUNTER++;
-=======
-public abstract class Quest : ScriptableObject
-{
-    [SerializeField] protected string questTitle;
-    [TextArea] [SerializeField] protected string questDescription;
-    [SerializeField] protected int questID;
-    [SerializeField] protected int experienceReward;
-    [SerializeField] protected QuestClearState questClearState;
-    protected bool isCompleted = false;
-    protected QuestButton questButton;
-    protected string questText;
-
-    public virtual void Init()
-    {
-        isCompleted = false;
->>>>>>> Main
     }
 
     public string GetQuestTitle()
@@ -42,7 +25,6 @@ public abstract class Quest : ScriptableObject
 
     public string GetQuestText()
     {
-        BuildQuestText();
         return questText;
     }
 
@@ -50,51 +32,4 @@ public abstract class Quest : ScriptableObject
     {
         return questID;
     }
-<<<<<<< HEAD
-=======
-
-    public bool IsCompleted()
-    {
-        return isCompleted;
-    }
-
-    public void SetButton(QuestButton qb)
-    {
-        questButton = qb;
-    }
-
-    protected virtual void BuildQuestText()
-    {
-        questText = questDescription;
-    }
-
-    public virtual void UpdateQuest()
-    {
-        questButton.UpdateQuestText(questText);
-    }
-
-    public virtual bool CompleteQuest()
-    {
-        if (isCompleted)
-        {
-            Game_Controller.UpdateWorldToQuestClearState(questClearState);
-            Game_Controller.GetPlayerInfo().ModifyExperience(experienceReward);
-        }
-        return isCompleted;
-    }
-
-    public abstract QuestType GetQuestType();
-}
-
-public enum QuestType
-{
-    QuestTypeError,
-    KILL_QUEST
-}
-
-public enum QuestClearState
-{
-    NONE,
-    OPEN_VILLAGE_EXIT
->>>>>>> Main
 }
