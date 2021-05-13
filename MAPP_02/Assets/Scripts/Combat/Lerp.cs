bd8719e33b04b3586a3fe21464a638426becd6b3
augustheden <aggeh@hotmail.se>
2021-05-13 15:00:02 +0200

    .

--- MAPP_02/Assets/Scripts/Combat/Lerp.cs
+++ MAPP_02/Assets/Scripts/Combat/Lerp.cs
@@ -1,44 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Lerp : MonoBehaviour
 {
 
-    private bool shouldLerp = false;
-
-    public float timeStartedLerping;
-    public float lerpTime;
+    private bool shouldLerp = false; 
 
     public Vector3 startPos;
     public Vector3 endPos;
-    private void StartLerping()
-    {
-        timeStartedLerping = Time.time;
-        shouldLerp = true;
-    }
     
     public void GoLerp()
     {
-        StartLerping();
+        shouldLerp = true;
     }
 
     void Update()
     {
         if (shouldLerp)
         {
-            transform.position = Lerping(startPos, endPos, timeStartedLerping, lerpTime);
-        }         
+            StartCoroutine(moveChar(startPos, endPos));
+        }
     }
 
+    private IEnumerator moveChar(Vector3 start, Vector3 end)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < 2f)
+        {
+            transform.position = Vector3.Lerp(start, end, elapsedTime / 1);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        
+            transform.position = Vector3.Lerp(end, start, elapsedTime / 4);
+            elapsedTime += Time.deltaTime;
+            shouldLerp = false;
+            yield return null;
+        
+    }
 
+    /*
     public Vector3 Lerping (Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
     {
+        
         float timeSinceStarted = Time.time - timeStartedLerping;
         float percentageComplete = timeSinceStarted / lerpTime;
         var result = Vector3.Lerp(start, end, percentageComplete);
+        
+        
 
         return result;
 
-    }
+    }*/
 }
