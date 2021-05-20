38e92c26f74fe0fcfa2949a6b329d89e62b37ca5
munchosaurus <martinkings1@live.se>
2021-05-20 10:00:55 +0200

    Added level blocker for quest 1

--- MAPP_02/Assets/Scripts/World/Quest_MinorCrystal.cs
+++ MAPP_02/Assets/Scripts/World/Quest_MinorCrystal.cs
@@ -7,6 +7,7 @@ public class Quest_MinorCrystal : NPC_Info
     [SerializeField] protected Quest quest;
     [TextArea] [SerializeField] protected string[] dialogueQuest;
     [SerializeField] GameObject objectToActivate;
+    [SerializeField] GameObject objectBlocking;
 
     override
     public void Interact()
@@ -16,12 +17,11 @@ public class Quest_MinorCrystal : NPC_Info
         {
             dialogue = dialogueQuest;
             objectToActivate.SetActive(true);
-            GameObject.Find("Minor Crystal").SetActive(false);
+            objectBlocking.SetActive(false);
+            gameObject.SetActive(false);
+            //GameObject.Find("Minor Crystal").SetActive(false);
         }
         base.Interact();
-        //if (Game_Controller.GetQuestLog().HasQuest(quest)) 
-        //{
 
-        //}
     }
 }
