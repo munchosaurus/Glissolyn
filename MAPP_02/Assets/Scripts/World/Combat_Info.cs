d15028daaa0ad800c7522d85090b32ac149ff0a6
Simon Täcklind <simon.tacklind@gmail.com>
2021-05-25 11:30:13 +0200

    Stuff

--- MAPP_02/Assets/Scripts/World/Combat_Info.cs
+++ MAPP_02/Assets/Scripts/World/Combat_Info.cs
@@ -38,8 +38,7 @@ public static class Combat_Info
     public static void PlayerWins()
     {
         Game_Controller.GetQuestLog().UpdateKillQuestsWithEnemyType(enemy.GetBase());
-        Game_Controller.GetDataBase().StartEnemyRespawnTimer(enemy);
-        enemy.gameObject.SetActive(false);
+        enemy.Die();
         int xpGained = enemy.GetBase().GetExperienceBase() * ((enemy.GetLevel() * 2) / Game_Controller.GetPlayerInfo().GetPlayerLevel());
         Game_Controller.GetPlayerInfo().ModifyExperience(xpGained);
         Game_Controller.GetDialogueBox().UpdateDialogue(new string[] { "You gained " + xpGained + " experience!"});
--- MAPP_02/Assets/Scripts/World/Enemy_Info.cs
+++ MAPP_02/Assets/Scripts/World/Enemy_Info.cs
@@ -4,7 +4,22 @@ public class Enemy_Info : NPC_Info
 {
     [SerializeField] CharacterBase Base;
     [SerializeField] int level;
-    
+
+    private float timer;
+    private bool dead;
+
+    private void Update()
+    {
+        if (dead)
+        {
+            timer -= Time.deltaTime;
+
+            if(timer <= 0)
+            {
+                Respawn();
+            }
+        }
+    }
 
     override
     public void Interact()
@@ -20,5 +35,20 @@ public class Enemy_Info : NPC_Info
     public int GetLevel()
     {
         return level;
+    }
+
+    public void Die()
+    {
+        dead = true;
+        timer = 30;
+        transform.position = new Vector3(transform.position.x, transform.position.y, -20);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+    
+    public void Respawn()
+    {
+        dead = false;
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
     }
 }
