e94801582ed1e7266cbd8c1ea54adbfe523f677b
MartinWallmark <70527220+MartinWallmark@users.noreply.github.com>
2021-05-27 22:39:09 +0200

    fixes

--- MAPP_02/Assets/Scripts/World/Last_Crystal.cs
+++ MAPP_02/Assets/Scripts/World/Last_Crystal.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Last_Crystal : MonoBehaviour
+public class Last_Crystal : NPC_Info
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private int interactCounter;
+    [SerializeField] GameObject witch;
+    [SerializeField] protected Quest quest;
+    [TextArea] [SerializeField] protected string[] dialog;
+    [SerializeField] GameObject player;
+
+
+    public void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        witch.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+
+    override
+    public void Interact()
+    {
+
+        if (Game_Controller.GetQuestLog().HasQuest(quest))
+        {
+
+
+            if (interactCounter == 0)
+            {
+                witch.SetActive(true);
+                dialog = dialogue;
+                Vector3 distanceDifference = new Vector3(1, 1);
+
+                witch.transform.position = player.transform.position - distanceDifference;
+                interactCounter++;
+            }
+                
+            
+            else
+            {
+                this.gameObject.SetActive(false);
+            }
+           
+        }
+        base.Interact();
+
+    }
+
+    private void ActivateWitch()
     {
         
+
     }
+
+
+    
 }
